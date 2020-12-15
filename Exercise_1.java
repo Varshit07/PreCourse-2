dc2b8640435952259427a5bc679b710305f05d8a
Varshit Shah <varshitshah97@gmail.com>
2020-12-15 11:43:23 -0800

    Completed Exercise 1
    implemented recursive version of binary search

--- Exercise_1.java
+++ Exercise_1.java
@@ -2,7 +2,25 @@ class BinarySearch {
     // Returns index of x if it is present in arr[l.. r], else return -1 
     int binarySearch(int arr[], int l, int r, int x) 
     { 
-        //Write your code here
+        // check if search space is valid
+		if (l <= r) { 
+          // calculate mid to avoid integer addition overflow  
+          int mid = l + (r - l) / 2; 
+
+          // Check if the element is at mid if yes then return mid
+          if (arr[mid] == x) 
+              return mid; 
+
+          // if x is on the right of arr[mid] then discard left half
+          if (x > arr[mid]) 
+              return binarySearch(arr, mid + 1, r, x);
+
+          // if x is on the left side of arr[mid] then discard right half
+          return binarySearch(arr, l, mid - 1, x); 
+        } 
+
+        // if x is not present in arr[]
+        return -1;
     } 
   
     // Driver method to test above 
